dce2d5baac2d714f0598858689d75edeb4df0384
Long Luu <longluu@Luus-MacBook-Pro.local>
2022-10-13 00:32:10 +0700

    RpaProcess

--- components/home/RpaProcess.js
+++ components/home/RpaProcess.js
@@ -1,5 +1,7 @@
 import React from "react";
 import step1Frame from "../../public/step1Frame.png";
+import arrowIcon from "../../public/arrowIcon.png";
+
 import Image from "next/image";
 import className from "classnames/bind";
 import styles from "../../styles/Home.module.scss";
@@ -9,16 +11,80 @@ export const RpaProcess = () => {
   return (
     <div className={cx("wrap")}>
       <h1 className={cx("title")}>RPA Process</h1>
-      <div className={cx("process")}>
+      <div className={cx("process1")}>
         <div className={cx("step1")}>
-          <Image src={step1Frame} alt="step1" layout="fixed" />
-          <div className={cx("content")} styles="background-image:{step1Frame}">
-            <h1>UNDERSTAND</h1>
+          <h1>UNDERSTAND</h1>
+          <div className={cx("content")}>
             <ul>
               <li>Understand the working process of clients</li>
               <li>Collect information of tools and effort of each task</li>
             </ul>
           </div>
+        </div>
+        <div className={cx("step2")}>
+          <h1>ANALYZE</h1>
+          <div className={cx("arrowIcon")}>
+            <Image src={arrowIcon} alt="arrowIcon" />
+          </div>
+
+          <div className={cx("content2")}>
+            <ul>
+              <li>
+                Analyze clients&apos; system, break down processes and identify
+                where to automate
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={cx("step3")}>
+          <h1>Structure</h1>
+          <div className={cx("arrowIcon")}>
+            <Image src={arrowIcon} alt="arrowIcon" />
+          </div>
+          <div className={cx("content3")}>
+            <ul>
+              <li>Listdown all parts of the process those can be automated</li>
+            </ul>
+          </div>
+        </div>
+        <div className={cx("step4")}>
+          <h1>Proposal</h1>
+          <div className={cx("arrowIcon")}>
+            <Image src={arrowIcon} alt="arrowIcon" />
+          </div>
+          <div className={cx("content4")}>
+            <ul>
+              <li>Propose the solutions for automation</li>
+              <li>Provide quotation</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className={cx("process2")}>
+        <div className={cx("frame")}></div>
+        <div className={cx("frame")}>
+          <ul>
+            <li>Image</li>
+            <li>Video</li>
+            <li>Voice</li>
+            <li>Text</li>
+          </ul>
+        </div>
+        <div className={cx("frame")}>
+          <ul>
+            <li>Document Parser</li>
+            <li>ID card Parser</li>
+            <li>License parser</li>
+            <li>OCR</li>
+            <li>Video analytic</li>
+          </ul>
+        </div>
+        <div className={cx("frame")}>
+          <ul>
+            <li>Options</li>
+            <li>Benefits</li>
+            <li>Trial/Production</li>
+          </ul>
         </div>
       </div>
     </div>
