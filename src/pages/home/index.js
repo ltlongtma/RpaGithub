826a84ab1cd558511d39345a425a260540e3a18a
Long Luu <ltlong@tma.com.vn>
2022-10-20 09:44:45 +0700

    refactor source code and start responsive

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,22 +1,18 @@
 import Head from "next/head";
-import { HighLight } from './components/HighLight/index';
-import { Overview } from './components/Overview/index';
-import { AIML } from './components/AIML/index';
-import { RpaProcess } from './components/Process/index';
-import { Solutions } from './components/Solutions/index';
-import { CaseStudy } from './components/CaseStudy/index';
-import { Contact } from './components/Contact/index';
-import styles from "./home.module.scss"
+import { HighLight } from "./components/HighLight/index";
+import { Overview } from "./components/Overview/index";
+import { AIML } from "./components/AIML/index";
+import { RpaProcess } from "./components/Process/index";
+import { Solutions } from "./components/Solutions/index";
+import { CaseStudy } from "./components/CaseStudy/index";
+import { Contact } from "./components/Contact/index";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>RPA_TMA</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
       </Head>
       <main>
         <div className={styles.container}>
