e0f14a83cbf5831ded9043fb1eadd5d185ac52d9
Long Luu <longluu@192.168.102.7>
2022-10-21 07:25:54 +0700

    responsive highLight

--- src/pages/home/components/HighLight/index.js
+++ src/pages/home/components/HighLight/index.js
@@ -16,66 +16,64 @@ const cx = className.bind(styles);
 
 export const HighLight = () => {
   return (
-      <div className={cx("container")}>
-        <div className={cx("imgBackground")}>
-          {/* <Image
-            src={backgroundHighLight}
-            alt="tma-logo"
-            priority
-          /> */}
-          <div className={cx("card")}>
-            <div className={cx("cardItem")}>
-              <div className={cx("cardIcon")}>
-                <Image src={energyControlIcon} alt="energyControlIcon" />
-              </div>
-              <div className={cx("cardContent")}>
-                <h1>85%</h1>
-                <p>Effiency gain</p>
-              </div>
+    <div className={cx("container")}>
+      <div className={cx("imgBackground")}>
+        <Image src={backgroundHighLight} alt="tma-logo" priority />
+        <div className={cx("card")}>
+          <div className={cx("cardItem")}>
+            <div className={cx("cardIcon")}>
+              <Image src={energyControlIcon} alt="energyControlIcon" />
             </div>
-            <div className={cx("cardItem")}>
-              <div className={cx("cardIcon")}>
-                <Image src={clockIcon} alt="clockIcon" />
-              </div>
-              <div className={cx("cardContent")}>
-                <h1>90%</h1>
-                <p>Reduce of processing time</p>
-              </div>
-            </div>
-            <div className={cx("cardItem")}>
-              <div className={cx("cardIcon")}>
-                <Image src={saveMoneyicon} alt="saveMoneyicon" />
-              </div>
-              <div className={cx("cardContent")}>
-                <h1>60%</h1>
-                <p>Save operating cost</p>
-              </div>
-            </div>
-            <div className={cx("cardItem")}>
-              <div className={cx("cardIcon")}>
-                <Image src={cogWheel} alt="cogWheel" />
-              </div>
-              <div className={cx("cardContent")}>
-                <h1>70%</h1>
-                <p>Reduce human effort</p>
-              </div>
+            <div className={cx("cardContent")}>
+              <h1>85%</h1>
+              <p>Effiency gain</p>
             </div>
           </div>
-          <div className={cx("content")}>
-            <div className={cx("left")}>
-              <Image src={iconGroupHighLight} alt="iconGroupHighLight" />
+          <div className={cx("cardItem")}>
+            <div className={cx("cardIcon")}>
+              <Image src={clockIcon} alt="clockIcon" />
             </div>
-            <div className={cx("right")}>
-              <h1 className={cx("title")}>TMA RPA Center</h1>
-              <div className={cx("body")}>
-                <p>Everything can be automated</p>
-                <p>Arm all of your employees with virtual assistant</p>
-                <p>Propelling digital transformation forward</p>
-                <p>Your processes are now more intelligent with AI</p>
-              </div>
+            <div className={cx("cardContent")}>
+              <h1>90%</h1>
+              <p>
+                Reduce of<br/> processing time
+              </p>
             </div>
           </div>
-          <div className={cx("menuBar")}>
+          <div className={cx("cardItem")}>
+            <div className={cx("cardIcon")}>
+              <Image src={saveMoneyicon} alt="saveMoneyicon" />
+            </div>
+            <div className={cx("cardContent")}>
+              <h1>60%</h1>
+              <p>Save operating cost</p>
+            </div>
+          </div>
+          <div className={cx("cardItem")}>
+            <div className={cx("cardIcon")}>
+              <Image src={cogWheel} alt="cogWheel" />
+            </div>
+            <div className={cx("cardContent")}>
+              <h1>70%</h1>
+              <p>Reduce human effort</p>
+            </div>
+          </div>
+        </div>
+        <div className={cx("content")}>
+          <div className={cx("left")}>
+            <Image src={iconGroupHighLight} alt="iconGroupHighLight" />
+          </div>
+          <div className={cx("right")}>
+            <h1 className={cx("title")}>TMA RPA Center</h1>
+            <div className={cx("body")}>
+              <p>Everything can be automated</p>
+              <p>Arm all of your employees with virtual assistant</p>
+              <p>Propelling digital transformation forward</p>
+              <p>Your processes are now more intelligent with AI</p>
+            </div>
+          </div>
+        </div>
+        <div className={cx("menuBar")}>
           <Image src={TmaLogo} alt="tma-logo" />
           <div className={cx("navigate")}>
             <Button>Home</Button>
@@ -99,10 +97,7 @@ export const HighLight = () => {
             <Button variant="contained">Contact Us</Button>
           </div>
         </div>
-        </div>
-       
-
-       
       </div>
+    </div>
   );
 };
